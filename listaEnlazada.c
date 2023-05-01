fa08da58ea4ef5505c5d745155606cbaf53f3ef9
julietacolque <yuliicolque@gmail.com>
2023-05-01 19:33:53 -0300

    practico 5

--- listaEnlazada.c
+++ listaEnlazada.c
@@ -27,24 +27,28 @@ Lista crearListaVacia();
 struct Nodo *crearNodo(Lista listaTareas, int indice);
 struct Nodo *quitarNodo(Lista *lista, int indice);
 Lista agregarTarea(Lista TPendientes);
-Lista insertarTarea(Lista TRealizadas, struct Nodo *nodo);
+Lista insertarTarea(Lista lista, struct Nodo *nodo);
 void listarTareas(Lista listadoTareas);
 void controlTareas(Lista *TPendientes, Lista *TRealizadas, int longitud);
-Tarea *buscarTareaId(Lista TPendientes, Lista TRealizadas, int idBuscado);
-Tarea *buscarTareaPorPalabra(Lista TPendientes, Lista TRealizadas, char *palabra);
+Tarea *buscarTareaId(Lista lista, int idBuscado);
+Tarea *buscarTareaPorPalabra(Lista lista, char *palabra);
+void eliminarTarea(struct Nodo *tarea);
+void mostrarDatos(Lista lista);
+void moverTareasTP5(Lista *TPendientes, Lista *TRealizadas, Lista *TProceso);
 int main()
 {
     srand(time(NULL));
     int longitud, opcion, salir = 1, id;
     char *palabra;
     Tarea *tareaBuscada;
-    Lista TPendientes, TRealizadas;
+    Lista TPendientes, TRealizadas, TProceso;
 
     TPendientes = crearListaVacia();
     TRealizadas = crearListaVacia();
+    TProceso = crearListaVacia();
     while (salir != 0)
     {
-        printf("\n\n");
+        printf("\n<<<<<<MENU>>>>>>>\n");
         opcion = menu();
 
         switch (opcion)
@@ -54,37 +58,45 @@ int main()
             break;
 
         case 2:
-            printf("\n\n TAREAS PENDIENTES \n\n");
+            printf("\n TAREAS PENDIENTES \n");
             listarTareas(TPendientes);
             break;
         case 3:
-            printf("\n\n TAREAS REALIZADAS \n\n");
+            printf("\nTAREAS REALIZADAS\n");
             listarTareas(TRealizadas);
             break;
 
         case 4:
-            printf("\n\n CONTROL DE TAREAS \n\n");
+            printf("\nCONTROL DE TAREAS\n");
             longitud = longitudDListaDT(TPendientes);
             controlTareas(&TPendientes, &TRealizadas, longitud);
             break;
         case 5:
-            printf("\n\n BUSQUEDA DE TAREAS POR ID \n\n");
+            printf("\nBUSQUEDA DE TAREAS POR ID\n");
             printf("Ingrese el id que busca: ");
             scanf("%d", &id);
-            tareaBuscada = buscarTareaId(TPendientes, TRealizadas, id);
+            tareaBuscada = buscarTareaId(TPendientes, id);
             (tareaBuscada) ? printf("\nLa tarea buscada con id: %d \n", id), mostrarTarea(*tareaBuscada) : printf("\nNo se encontro la tarea");
             break;
         case 6:
-            printf("\n\n BUSQUEDA DE TAREAS POR PALABRA \n\n");
+            printf("\nBUSQUEDA DE TAREAS POR PALABRA\n");
             palabra = malloc(sizeof(char) * 100);
             printf("\nIngrese una palabra: ");
             fflush(stdin);
             gets(palabra);
-            tareaBuscada = buscarTareaPorPalabra(TPendientes, TRealizadas, palabra);
+            tareaBuscada = buscarTareaPorPalabra(TPendientes, palabra);
             (tareaBuscada) ? printf("\nLa tarea buscada por palabra %s \n", palabra), mostrarTarea(*tareaBuscada) : printf("\nNo se encontro la tarea");
+            free(palabra);
+            break;
+        case 7:
+            mostrarDatos(TPendientes);
+            break;
+        case 8:
+            moverTareasTP5(&TPendientes, &TRealizadas, &TProceso);
             break;
         }
-        printf("\n\nDesea seguir realizando operaciones? 1-SI 0-NO: ");
+
+        printf("\nDesea seguir realizando operaciones? 1-SI 0-NO: ");
         scanf("%d", &salir);
     }
 
@@ -102,11 +114,12 @@ int menu()
         printf("4-Control de tareas\n");
         printf("5-Busqueda de tareas por ID\n");
         printf("6-Busqueda de tareas por Palabra\n");
+        printf("7-Mostrar datos\n");
+        printf("8-Mover tareasTP5\n");
         printf("Ingrese la opcion: ");
         scanf("%d", &respuesta);
-        printf("\n\n");
 
-    } while (respuesta != 1 && respuesta != 2 && respuesta != 3 && respuesta != 4 && respuesta != 5 && respuesta != 6);
+    } while (respuesta != 1 && respuesta != 2 && respuesta != 3 && respuesta != 4 && respuesta != 5 && respuesta != 6 && respuesta != 7 && respuesta != 8);
 
     return respuesta;
 }
@@ -133,13 +146,14 @@ int longitudDListaDT(Lista L)
     return cantidad;
 }
 Tarea crearTarea(int indice)
-{
+{   
+    printf("\n<<<<CREANDO TAREA>>>>\n");
     char *descripcion;
     Tarea nuevaTarea;
     descripcion = malloc(sizeof(char) * 100);
     nuevaTarea.TareaID = indice;
     fflush(stdin);
-    printf("Ingrese la descripcion de la tarea: ");
+    printf("\nIngrese la descripcion de la tarea: ");
     gets(descripcion);
     nuevaTarea.Descripcion = malloc(sizeof(char) * 100);
     strcpy(nuevaTarea.Descripcion, descripcion);
@@ -211,12 +225,12 @@ Lista agregarTarea(Lista TPendientes)
     return (TPendientes);
 }
 
-Lista insertarTarea(Lista TRealizadas, struct Nodo *nodo)
+Lista insertarTarea(Lista lista, struct Nodo *nodo)
 {
 
-    nodo->Siguiente = TRealizadas;
-    TRealizadas = nodo;
-    return (TRealizadas);
+    nodo->Siguiente = lista;
+    lista = nodo;
+    return (lista);
 }
 
 void listarTareas(Lista listadoTareas)
@@ -268,54 +282,152 @@ void controlTareas(Lista *TPendientes, Lista *TRealizadas, int longitud)
         }
         // aux perdio la linea de la lista de tareas por ello se produjo un error de segmentacion porque una vezz que quite el primer nodo se perdio la conexion con el resto pero Tpendientes mantiene la conexion porque la funcion quitar nodo la conserva gracias a la modificacion que se le hace, por lo tanto, cuando a aux le redirecciono que apunte al nodo 2 ya lo recorre sin problema
     }
-      else {
-            printf("\n No hay tareas pendientes\n\n");
-        }
+    else
+    {
+        printf("\n No hay tareas pendientes\n\n");
+    }
 }
-Tarea *buscarTareaId(Lista TPendientes, Lista TRealizadas, int idBuscado)
+Tarea *buscarTareaId(Lista lista, int idBuscado)
 {
 
-    while (!esListaVacia(TPendientes) && TPendientes->T.TareaID != idBuscado)
+    while (!esListaVacia(lista))
     {
-        TPendientes = TPendientes->Siguiente;
-    }
-    if (TPendientes)
-    {
-        return (&(TPendientes)->T);
-    }
-
-    while (!esListaVacia(TRealizadas) && TRealizadas->T.TareaID != idBuscado)
-    {
-        TRealizadas = TRealizadas->Siguiente;
-    }
-    if (TRealizadas)
-    {
-        return (&(TRealizadas)->T);
+        if (lista->T.TareaID == idBuscado)
+        {
+            return (&(lista)->T);
+        }
+        lista = lista->Siguiente;
     }
     return NULL;
 }
 
-Tarea *buscarTareaPorPalabra(Lista TPendientes, Lista TRealizadas, char *palabra)
+Tarea *buscarTareaPorPalabra(Lista lista, char *palabra)
 {
 
-    while (!esListaVacia(TPendientes))
+    while (!esListaVacia(lista))
     {
-        if (strstr(TPendientes->T.Descripcion, palabra))
+        if (strstr(lista->T.Descripcion, palabra))
         {
-            return (&(TPendientes)->T);
+            return (&(lista)->T);
         }
 
-        TPendientes = TPendientes->Siguiente;
-    }
-    while (!esListaVacia(TRealizadas))
-    {
-        if (strstr(TRealizadas->T.Descripcion, palabra))
-        {
-            return (&(TRealizadas)->T);
-        }
-
-        TRealizadas = TRealizadas->Siguiente;
+        lista = lista->Siguiente;
     }
 
     return NULL;
+}
+
+void eliminarTarea(struct Nodo *tarea)
+{
+    free(tarea->T.Descripcion);
+    free(tarea);
+}
+
+void mostrarDatos(Lista lista)
+{
+    if (!esListaVacia(lista))
+    {
+        int longitud = longitudDListaDT(lista), sumaDuracion = 0;
+        for (int i = 0; i < longitud; i++)
+        {
+            sumaDuracion += lista->T.Duracion;
+            lista = lista->Siguiente;
+        }
+        printf("La longitud de la lista es: %d \n", longitud);
+        printf("El tiempo que tomara realizar las tareas es: %d minutos \n", sumaDuracion);
+    }
+    else
+    {
+        printf("\nLa lista esta vacia.\n");
+    }
+}
+
+void moverTareasTP5(Lista *TPendientes, Lista *TRealizadas, Lista *TProceso)
+{
+    int id, lugarDTarea, opcionLugar, accion, salir = 1;
+    struct Nodo *tarea;
+
+    while (salir != 0)
+    {
+        printf("\n>>>>>LISTADO TAREAS PENDIENTES>>>>>>\n");
+        listarTareas(*TPendientes);
+        printf("\n>>>>>LISTADO TAREAS EN PROCESO >>>>>>\n");
+        listarTareas(*TProceso);
+        printf("\n>>>>>LISTADO TAREAS REALIZADAS>>>>>>\n");
+        listarTareas(*TRealizadas);
+
+        printf("Ingrese el id de la tarea que desea seleccionar: ");
+        scanf("%d", &id);
+        do
+        {
+            printf("\n1-Tareas Pendientes\n2-Tareas Realizadas\n3-Tareas en Proceso\nLa tarea se encuentra en: ");
+            scanf("%d", &lugarDTarea);
+        } while (lugarDTarea != 1 && lugarDTarea != 2 && lugarDTarea != 3);
+
+        do
+        {
+            printf("\n1-Mover tarea\n2-Eliminar Tarea\n3-Nada\nQue desea hacer con la tarea?: ");
+            scanf("%d", &accion);
+        } while (accion != 1 && accion != 2 && accion != 3);
+
+        if (accion != 3)
+        {
+            switch (lugarDTarea)
+            {
+            case 1:
+                tarea = quitarNodo(TPendientes, id);
+                break;
+            case 2:
+                tarea = quitarNodo(TRealizadas, id);
+                break;
+            case 3:
+                tarea = quitarNodo(TProceso, id);
+                break;
+            }
+        }
+
+        switch (accion)
+        {
+        case 1:
+            do
+            {
+                printf("\n1-TareasPendientes\n2-TareasRealizadas\n3-Tareas en Proceso\nA donde quiere mover la tarea?: ");
+                scanf("%d", &opcionLugar);
+            } while (lugarDTarea == opcionLugar);
+            if (opcionLugar == 1)
+            {
+                *TPendientes = insertarTarea(*TPendientes, tarea);
+            }
+            if (opcionLugar == 2)
+            {
+                *TRealizadas = insertarTarea(*TRealizadas, tarea);
+            }
+            if (opcionLugar == 3)
+            {
+                *TProceso = insertarTarea(*TProceso, tarea);
+            }
+
+            break;
+        case 2:
+            printf("\n\nELIMINANDO TAREA \n\n");
+            eliminarTarea(tarea);
+            break;
+        case 3:
+            printf("\nhaciendo nada...\n");
+            break;
+        }
+
+        printf("\nQuiere seguir realizando acciones 0-NO: ");
+        scanf("%d", &salir);
+    }
+
+    printf("\n>>>>>LISTADO TAREAS PENDIENTES>>>>>>\n");
+    listarTareas(*TPendientes);
+    mostrarDatos(*TPendientes);
+    printf("\n>>>>>LISTADO TAREAS EN PROCESO >>>>>>\n");
+    listarTareas(*TProceso);
+    mostrarDatos(*TProceso);
+    printf("\n>>>>>LISTADO TAREAS REALIZADAS>>>>>>\n");
+    listarTareas(*TRealizadas);
+    mostrarDatos(*TRealizadas);
 }
